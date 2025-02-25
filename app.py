b42ef272a95292ad30d9b2a941d36d8e78c84f2d
Andrey Sennikov <99654050+SennikovAndrey@users.noreply.github.com>
2025-02-26 01:23:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import streamlit as st
 
 from PIL import Image
 
-from backend.producers import producer_mnist, consumer_mnist
+from backend.producers_consumers import producer_mnist_inference, consumer_mnist_inference
+from backend.config_file import kafka_topic
 
 parent_dir = Path(__file__).resolve().parent.parent
 sys.path.append(str(parent_dir))
@@ -16,8 +17,8 @@ from backend.data import inference_transforms
 def main():
     st.title('MNIST Image Classification with Kafka')
 
-    producer = producer_mnist()
-    consumer = consumer_mnist()
+    producer = producer_mnist_inference()
+    consumer = consumer_mnist_inference()
 
     uploaded_file = st.file_uploader("Upload an MNIST image", type=["png", "jpg", "jpeg"])
     if uploaded_file is not None:
@@ -25,16 +26,16 @@ def main():
         image = image.resize((28, 28))
         image_tensor = inference_transforms(image)
 
-        producer.send_to_kafka('mnist_images', image_tensor)
+        producer.send_to_kafka(kafka_topic, image_tensor)
 
-        prediction_data = consumer.receive_from_kafka('mnist_images')
-        print('-' * 80)
-        print(prediction_data)
-        if prediction_data:
-            prediction_label = int(prediction_data)
-            print('-' * 80)
-            print(prediction_label)
-            st.write(f'Predicted Label: {prediction_label}')
+        prediction_data = consumer.receive_from_kafka(kafka_topic)
+        if prediction_data is not None:
+            if isinstance(prediction_data, int):
+                st.write(f'Predicted Label: {prediction_data}')
+            else:
+                st.write(f'Received data: {prediction_data}')
+        else:
+            st.write("No prediction received from Kafka.")
 
         st.image(image, caption='Uploaded Image', use_column_width=True)
 
